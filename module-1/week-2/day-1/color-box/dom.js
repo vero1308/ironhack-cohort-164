28e64b0cfd584f301e33565a9f11e869350f5291
meta-one <meta-one@meta-one.local>
2019-10-22 14:33:01 +0200

    added css transition on box-click + removed unused files

--- module-1/week-2/day-1/color-box/dom.js
+++ module-1/week-2/day-1/color-box/dom.js
@@ -3,20 +3,39 @@ const contentMain = document.getElementById("main_content");
 const boxBackground = document.getElementById("box_bg");
 const boxCount = document.getElementById("nb_blocks");
 
+function foo() {
+  console.log("foo");
+}
+
+function toggleActiveClass(evt) {
+  // classList API doc : https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
+  console.log(evt);
+  const element = evt.srcElement || evt.target;
+  element.classList.toggle("is-active");
+  console.log(element === this);
+}
+
 function addBox() {
   const box = document.createElement("div");
   const count = contentMain.querySelectorAll(".box").length;
   box.style.backgroundColor = boxBackground.value;
   box.className = "box";
+  // box.style.visibility = "hidden";
   box.textContent = count + 1;
-  box.onclick = toggleActiveClass;
-  contentMain.appendChild(box);
-}
+  // console.log(box);
 
-function toggleActiveClass(evt) {
-  // classList API doc : https://developer.mozilla.org/en-US/docs/Web/API/Element/classList
-  evt.target.classList.toggle("is-active");
-  console.log(evt.target === this);
+  // this line below is the longest form of line 25
+  // box.addEventListener("click", toggleActiveClass);
+  box.onclick = toggleActiveClass;
+
+  // you can add several listeners to an element
+  box.onmouseenter = function() {
+    // you may use anonymous function to wrap several actions
+    console.log("mouse enter my friends");
+    foo();
+  };
+
+  contentMain.appendChild(box);
 }
 
 function handleClick() {
@@ -27,10 +46,10 @@ function handleClick() {
   }
 }
 
-function $(sCSS) {
-  return document.querySelector(sCSS);
-}
-
-console.log("my JQuery :D", $("#nb_blocks"));
-
 btnCreate.onclick = handleClick;
+
+// function $(sCSS) {
+//   return document.querySelector(sCSS);
+// }
+
+// console.log("my JQuery :D", $("#nb_blocks"));
