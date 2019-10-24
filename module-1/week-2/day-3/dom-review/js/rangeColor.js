c7360504a418adda5cd803c5a180d7fcba8b5ad9
meta-one <meta-one@meta-one.local>
2019-10-24 11:14:53 +0200

    minor changes

--- module-1/week-2/day-3/dom-review/js/rangeColor.js
+++ module-1/week-2/day-3/dom-review/js/rangeColor.js
@@ -1,11 +1,10 @@
 const ranges = document.querySelectorAll(".range-color .range");
+const sumElement = document.getElementById("sum");
 
 
-function updateValue(evt) {
+function updateValue(input) {
   var activeClass;
-  const input = evt.target;
-//   const p = input.parentElement.querySelector(".value");
-  const p = input.previousElementSibling;
+  const p = input.parentElement.querySelector(".value");
   p.textContent = input.value;
   if (input.value < 33) activeClass = "danger";
   else if (input.value < 66) activeClass = "warning";
@@ -13,6 +12,29 @@ function updateValue(evt) {
   p.className = "value " + activeClass;
 }
 
+function displaySum(res) {
+  sumElement.textContent = res
+}
+
+function sum1() {
+  var tmp = 0;
+  ranges.forEach(range => {
+    tmp += Number(range.value);
+  });
+  return tmp;
+}
+
+function sum2() {
+  return Array.from(ranges).reduce((acc, range) => acc + Number(range.value), 0);
+}
+
+function handleInputs(evt) {
+  updateValue(evt.target);
+  displaySum(sum2());
+}
+
 ranges.forEach(range => {
-  range.oninput = updateValue;
+  // loop through each range
+  range.oninput = handleInputs; // listen to input events and handle them
 });
+
