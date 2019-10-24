8577569561c29397a69e13b827c026f9efa572d5
meta-one <meta-one@meta-one.local>
2019-10-24 11:14:28 +0200

    added AJAX I snippet (axios - get)

---
+++ module-1/week-2/day-4/ajax-axios-get/get-web-data.js
@@ -0,0 +1,29 @@
+// console.log(axios);
+
+const url =
+  "https://opendata.paris.fr/api/records/1.0/search/?dataset=arbresremarquablesparis&rows=1000&facet=genre&facet=espece&facet=stadedeveloppement&facet=varieteoucultivar&facet=dateplantation&facet=libellefrancais";
+
+const body = document.querySelector("body");
+
+function displayTrees(trees) {
+  // console.log(trees); // all the nice trees in Paris
+  body.innerHTML = "<ul>";
+  // .... loop through the trees
+  trees.forEach(tree => {
+    body.innerHTML += `<li>
+            ${tree.fields.espece} - 
+            ${tree.fields.adresse}
+        </li>`;
+  });
+  body.innerHTML += "</ul>";
+}
+
+axios
+  .get(url)
+  .then(res => {
+    // console.log(res);
+    displayTrees(res.data.records); // an array ojf objects
+  })
+  .catch(err => {
+    console.error(err);
+  });
