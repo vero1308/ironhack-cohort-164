f3909ded93d9bb4acbc27e7e065d3e35e83fcf11
meta-one <meta-one@meta-one.local>
2019-11-04 15:26:59 +0100

    minor updates

--- module-2/week-1/day-1/express-first-setup/server.js
+++ module-2/week-1/day-1/express-first-setup/server.js
@@ -6,7 +6,10 @@ const server = express();
 // console.log(server);
 const port = 8080;
 
-// console.log(__dirname); // node constant always available ... indicates the current file's absolute path 
+
+// console.log(global)
+
+// console.log(__dirname); // node constant always available ... indicates the current file's absolute path
 
 /** APP INITAL SETUP !!! MANDATORY **/
 
@@ -14,31 +17,55 @@ const port = 8080;
 server.use(express.static(path.join(__dirname, "public"))); // rock solid syntax
 
 // indicates express where our "views" templates are located
-server.set("views", path.join(__dirname, "views")); 
+server.set("views", path.join(__dirname, "views"));
 // absolute path to a folder called "views"
 
 // indicates express wich view engine this app will use ; )
 server.set("view engine", "hbs"); // in this case hbs, there are many others ... pug/jade etc.engine !
 
 server.get("/", (request, response) => {
-    // server-side rendering !!! 
-    // hbs will "prepare" (convert) the file into proper html before
-    // sending the response back top the client :)
+  // server-side rendering !!!
+  // hbs will "prepare" (convert) the file into proper html before
+  // sending the response back top the client :)
+
   response.render("home", {
-      title: "Hello Express",
-      doYouGetNode: false,
-      icon1: "<i class=\"fa fa-heart\"></i>",
-      icon2: "<i class=\"fa fa-code\"></i>",
+    title: "Hello Express",
+    doYouGetNode: true,
+    icon1: '<i class="fa fa-heart"></i>',
+    icon2: '<i class="fa fa-code"></i>',
+    css: ["home", "foo"]
   });
 });
 
 server.get("/about", (request, response) => {
-  response.render("about");
+  // pass a title variable to the about view and display it on the page !!!
+  const data = {
+    title: "foo",
+    students: ["Sarah", "Amine", "Louise", "Camilo", "Parina", "Evgeny"],
+    css: ["about"]
+  };
+  response.render("about", data);
 });
 
 server.get("/signup", (request, response) => {
-  response.render("signup");
+  const data = {
+    title: "Signup",
+    students: [
+      {name: "PH", cohort: 164},
+      {name: "Nico", cohort: 164},
+      {name: "Venkat", cohort: 164}
+    ]
+  }
+  response.render("signup", data);
 });
+
+server.get("/contact",  (req, res) => {
+
+  res.render("contact", {
+    title: "Contacto"
+  })
+});
+
 
 server.listen(port);
 // hey server : listen to http req on this specific port !!!
