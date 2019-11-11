30b6624efa98abb46a056a4029843b9d50402608
meta-one <meta-one@meta-one.local>
2019-11-11 09:09:58 +0100

    added admin pivot (protected) route

--- module-2/week-1/day-4/artists-express/routes/base.js
+++ module-2/week-1/day-4/artists-express/routes/base.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = new express.Router();
-
+const protectAdminRoute = require("../middlewares/protectAdminRoute");
 /*------------------------------------------
 // BASE ROUTING
 ------------------------------------------*/
+
+// PUBLIC SITE
 
 router.get("/", (req, res) => res.redirect("/home"));
 
@@ -11,22 +13,19 @@ router.get("/home", (req, res) => {
   res.render("home");
 });
 
-router.get("/search", (req, res) => {
-  const searchResults = ["wutang", "toto", "u2"]
-  res.json(searchResults);
-});
-
-router.post("/comment", (req, res) => {
-  // here you just got a post req ..
-  console.log(req.body);
-});
-
-// router.get("/about", (req, res) => {
-//   res.render("home");
-// });
-
 // router.get("/contact", (req, res) => {
 //   res.render("home");
 // });
+
+// router.get("/search", (req, res) => {
+//   const searchResults = ["wutang", "toto", "u2"];
+//   res.json(searchResults);
+// });
+
+// BACKEND SITE
+
+router.get("/admin", protectAdminRoute, (req, res) => {
+  res.render("admin");
+});
 
 module.exports = router;
