57e2b45d2fb781ce221b1a3ce57492697f502d0b
meta-one <meta-one@meta-one.local>
2019-11-11 09:04:30 +0100

    added protected route feature + album.styles populate()

--- module-2/week-1/day-4/artists-express/routes/album.js
+++ module-2/week-1/day-4/artists-express/routes/album.js
@@ -1,43 +1,33 @@
+/*------------------------------------------
+// ALBUMS ROUTING
+------------------------------------------*/
+
 const express = require("express");
 const router = new express.Router();
 const albumModel = require("./../models/Album");
+const artistModel = require("./../models/Artist");
+const protectAdminRoute = require("./../middlewares/protectAdminRoute");
 
-router.get("/create-album", (req, res) => {
-  res.render("form-album");
-});
+// PUBLIC ROUTES
 
 router.get("/all-albums", (req, res) => {
   albumModel
     .find() // this will fetch all albums from db
+    .populate({
+      path: "artist",
+      populate: {
+        path: "style"
+      }
+    }) // the artist key is a reference (objectId) to another document in the artists collection
     .then(dbRes => {
       // if successfull, it's an array of albums
       // console.log(dbRes)
-      res.render("albums", { albums: dbRes }); // pass the array to the view
+      res.render("albums", { albums: dbRes, css: ["album"] }); // pass the array to the view
     })
     .catch(dbErr => {
-      // if an error occured
+      // else... catch if an error occured
       console.log("err", dbErr);
     });
-});
-
-router.get("/album-edit/:id", (req, res) => {
-  albumModel
-    .findOne({ _id: { $eq: req.params.id } }) // this will fetch one album by id from db
-    .then(dbRes => {
-      // if successfull, it's an objet representing an albums
-      res.render("form-album-edit", { album: dbRes });
-    })
-    .catch(dbErr => {
-      // if an error occured
-      console.log("err", dbErr);
-    });
-});
-
-router.get("/album-delete/:id", (req, res) => {
-  albumModel
-    .findByIdAndDelete(req.params.id)
-    .then(dbRes => res.redirect("/all-albums"))
-    .catch(dbErr => console.log("err", dbErr));
 });
 
 router.get("/album/:id", (req, res) => {
@@ -56,13 +46,24 @@ router.get("/album/:id", (req, res) => {
     });
 });
 
-router.get("/all-albums", (req, res) => {
-  albumModel
-    .find() // this will fetch all albums from db
+// BACKEND ROUTES
+router.get("/create-album", protectAdminRoute, (req, res) => {
+  artistModel
+    .find()
     .then(dbRes => {
-      // if successfull, it's an array of albums
-      // console.log(dbRes)
-      res.render("albums", { albums: dbRes }); // pass the array to the view
+      res.render("form-album", {artists: dbRes});
+    })
+    .catch(dbErr => {
+      console.error(dbErr);
+    });
+});
+
+router.get("/edit-album/:id", protectAdminRoute, (req, res) => {
+  albumModel
+    .findOne({ _id: { $eq: req.params.id } }) // this will fetch one album by id from db
+    .then(dbRes => {
+      // if successfull, it's an objet representing an albums
+      res.render("form-album-edit", { album: dbRes });
     })
     .catch(dbErr => {
       // if an error occured
@@ -70,13 +71,30 @@ router.get("/all-albums", (req, res) => {
     });
 });
 
-router.post("/create-album", (req, res) => {
+router.get("/manage-albums", protectAdminRoute, (req, res) => {
+  albumModel
+    .find()
+    .populate("artist")
+    .then(dbRes => {
+      res.render("manage-albums", { albums: dbRes, css: ["tabler"] });
+    })
+    .catch(dbErr => console.log("err", dbErr));
+});
+
+router.get("/delete-album/:id", protectAdminRoute, (req, res) => {
+  albumModel
+    .findByIdAndDelete(req.params.id)
+    .then(dbRes => res.redirect("/manage-albums"))
+    .catch(dbErr => console.log("err", dbErr));
+});
+
+router.post("/create-album", protectAdminRoute, (req, res) => {
   albumModel
     .create(req.body) // use the model and try doc insertion in database
     .then(() => {
       // if successfull
       // console.log("res", dbRes); // the created movie
-      res.redirect("/all-albums"); // redirect ALLWAYS Triggers a GET request
+      res.redirect("/manage-albums"); // redirect ALLWAYS Triggers a GET request
     })
     .catch(dbErr => {
       // if an error occured
@@ -84,7 +102,7 @@ router.post("/create-album", (req, res) => {
     });
 });
 
-router.post("/edit-album/:id", (req, res) => {
+router.post("/edit-album/:id", protectAdminRoute, (req, res) => {
   albumModel
     .findByIdAndUpdate(req.params.id, req.body)
     .then(dbRes => res.redirect("/all-albums"))
