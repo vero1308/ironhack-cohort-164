9b0a845cf60a95cd68f8a9706b01d10d0d72f3e7
meta-one <meta-one@meta-one.local>
2019-11-12 09:40:35 +0100

    remplace user with User @ model route

--- module-2/week-2/day-1/artists-express-next/routes/auth.js
+++ module-2/week-2/day-1/artists-express-next/routes/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const userModel = require("../models/user");
+const userModel = require("../models/User");
 const bcrypt = require("bcrypt");
 
 //Authorization
