d797492fcc4a58db6f8eba3538efc35e3ead7af8
meta-one <meta-one@meta-one.local>
2019-11-13 09:06:35 +0100

    added avatr field

--- module-2/week-2/day-1/artists-express-next/models/User.js
+++ module-2/week-2/day-1/artists-express-next/models/User.js
@@ -2,14 +2,17 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  // username: String,
   email: String,
   password: String,
   role: {
     type: String,
     enum: ["admin", "editor", "user"],
     default: "user"
-  }
+  },
+  avatar: {
+    type: String,
+    default: "https://cdn.onlinewebfonts.com/svg/img_258083.png"
+  } 
 });
 
 const userModel = mongoose.model("User", userSchema);
